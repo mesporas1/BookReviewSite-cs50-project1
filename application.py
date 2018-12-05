958cf6d479a0046b09b840b6c9472fb6a067e14b
Mathew <24691566+mesporas1@users.noreply.github.com>
2018-12-05 00:23:58 -0800

    Added getting search results for books
    User is able to enter ISBN, title, or author in order to grab book data. Currently, a user can enter from any of the three fields and still be able to query the results.

--- application.py
+++ application.py
@@ -49,8 +49,8 @@ def success():
     
     
 
-@app.route("/login", methods=["POST"])
-def login():
+@app.route("/search", methods=["POST"])
+def search():
     # Get username and password
     username = request.form.get("username")
     password = request.form.get("password")
@@ -59,11 +59,29 @@ def login():
     if db.execute("SELECT * FROM users WHERE username = :username AND password = :password",{"username": username, "password": password}).rowcount == 0:
         return render_template("error.html", message = "No such username and password exists")
     else:
-        return render_template("books.html", user = username)
+        return render_template("search.html", user = username)
 
+@app.route("/search/results", methods=["POST"])
+def books():
+    """Lists books from the search"""
+    isbn = request.form.get("isbn")
+    title = request.form.get("title")
+    author = request.form.get("author")
+    
+    if isbn != "":
+        isbn = "%" + request.form.get("isbn") + "%"
+    if title != "":
+        title = "%" + request.form.get("title") + "%"
+    if author != "":
+        author = "%" + request.form.get("author") + "%"
 
-#@app.route("/books")
-#def books():
-#    """Lists all flights."""
-#    books = db.execute("SELECT * FROM books").fetchall()
-#    return render_template("flights.html", flights=flights)
+    #Make sure book exists
+    books = db.execute("SELECT * FROM books WHERE isbn LIKE :isbn OR title LIKE :title OR author LIKE :author", {"isbn": isbn, "title":title, "author":author}).fetchall()
+    if books is None:
+        return render_template("error.html", message = "No books")
+    return render_template("books.html", books = books, isbn = isbn, title = title, author = author)
+
+    #books = db.execute("SELECT * FROM books WHERE isbn = :isbn", {"isbn": isbn}).fetchall()
+    #if books is None:
+    #    return render_template("error.html", message = "No books")
+    #return render_template("books.html", books = books)
